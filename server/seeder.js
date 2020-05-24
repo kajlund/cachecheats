d82fc8428706da64c9bd93e4edded01b520bfd07
Kaj Lund <kaj.lund@gmail.com>
2020-05-24 09:40:06 +0300

    Add data seeder script

---
+++ server/seeder.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const dotenv = require('dotenv');
+const log = require('consola');
+const mongoose = require('mongoose');
+
+// load env vars
+dotenv.config();
+
+// Load models
+const User = require('./model/User');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const users = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), '_data', 'users.json'), 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await User.create(users);
+
+    log.success('Data imported...');
+    process.exit();
+  } catch (err) {
+    log.error(err);
+  }
+};
+
+// Delete Data
+const deleteData = async () => {
+  try {
+    await User.deleteMany();
+
+    log.success('Data destroyed...');
+    process.exit();
+  } catch (err) {
+    log.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
