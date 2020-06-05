c2cfd66fddeb5554a51173264a046e752dd5bbf0
Kaj Lund <kaj.lund@gmail.com>
2020-06-05 14:14:04 +0300

    Fix timestamps config

--- server/model/User.js
+++ server/model/User.js
@@ -3,40 +3,42 @@ const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is a required field'],
-    maxlength: [50, 'Name can not be longer than 50 characters'],
-    minlength: [4, 'Name must be at least 4 characters long'],
-    trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is a required field'],
+      maxlength: [50, 'Name can not be longer than 50 characters'],
+      minlength: [4, 'Name must be at least 4 characters long'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      lowercase: true,
+      unique: [true, 'Email must be unique'],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email address',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'The password must be at least 6 characters long'],
+      select: false, // Don't return pwd by default in queries
+    },
+    avatar: String,
+    role: {
+      type: String,
+      enum: ['', 'user', 'admin'],
+      default: '',
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    lowercase: true,
-    unique: [true, 'Email must be unique'],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email address',
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'The password must be at least 6 characters long'],
-    select: false, // Don't return pwd by default in queries
-  },
-  avatar: String,
-  role: {
-    type: String,
-    enum: ['', 'user', 'admin'],
-    default: '',
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  timestamps: Date,
-});
+  { timestamps: true }
+);
 
 UserSchema.pre('save', async function (next) {
   // Generate Avatar
